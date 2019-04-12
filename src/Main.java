6ff00e065d616c38f20df4db190c778e92e56059
DimaHudovec <hudovecd@gmail.com>
2019-04-12 12:23:42 +0300

    Add Hello method

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        hello("Dima");
+    }
+    public static void hello(String name){
+        System.out.println("Hello " + name);
     }
 }
