537dcfb1d40367cbdd4faede02de339812f0a2d6
DimaHudovec <hudovecd@gmail.com>
2019-04-12 15:04:38 +0300

    Add Hello method

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,16 @@
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        hello("Dima");
     }
     public static void hello(String name){
         System.out.println("Hello " + name);
+    }
+    public static void helloAll(ArrayList<String> names){
+        for (String name : names) {
+            hello(name);
+        }
     }
 }
