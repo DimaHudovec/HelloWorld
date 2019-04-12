1e75871df9d1df83e2d0f7f1f32d883637080ad4
DimaHudovec <hudovecd@gmail.com>
2019-04-12 16:40:30 +0300

    Add by method

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,10 @@ public class Main {
     public static void hello(String name){
         System.out.println("Hello " + name);
     }
+
+    public static void bye(String name){
+        System.out.println("Bye " + name);
+    }
     public static void helloAll(ArrayList<String> names){
         for (String name : names) {
             hello(name);
